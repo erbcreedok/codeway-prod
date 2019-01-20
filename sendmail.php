5bc4149b1ce5ed690c966be3b6a45629f037920e
erbcreedok <erbcreedok@gmail.com>
2019-01-20 06:17:19 +0600

    fix tg problem

--- sendmail.php
+++ sendmail.php
@@ -21,8 +21,6 @@ $mail->isHTML(true);                                  // Set email format to HTM
 $subject = $_GET['subject'];
 $message = $_GET['message'];
 
-file_put_contents("./sendtg.php?message=$message");
-
 $mail->Subject = $subject;
 $mail->Body    = $message;
 $mail->AltBody = $message;
@@ -32,4 +30,15 @@ if (!$mail->send()) {
 } else {
     echo 'success';
 };
-?>
+
+
+$botToken = '709640004:AAFGHsOpg2yPvDnAE7E6aEKXWScIcnjfOSY';
+$chatId = '-396860479';
+$message = urldecode($_GET['message']);
+$message = urlencode(str_replace("<br>", "\n", $message));
+$xml = file_get_contents(
+    "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&parse_mode=html&text=$message"
+);
+$xml = file_get_contents(
+    "https://api.telegram.org/bot$botToken/sendMessage?chat_id=254410503&parse_mode=html&text=$message"
+);
